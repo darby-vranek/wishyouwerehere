66ee6df3093909b7d109fa8f650fe3283cee4641
lyra <darby.vranek@gmail.com>
2021-05-08 19:39:00 -0700

    minimap icon is not showing up but anim etc are working

--- scripts/prefabs/wendst.lua
+++ scripts/prefabs/wendst.lua
@@ -15,13 +15,18 @@ local start_inv =
 
 local fn = function(inst)
 	inst.soundsname = "wendy"
+
+	-- inst.AnimState:SetBank("wendy")
+	inst.AnimState:SetBuild("wendy")
 	
 	local minimap = inst.entity:AddMiniMapEntity()
-	minimap:SetIcon( "minimap/wendst.tex" )
+	minimap:SetIcon( "wendst.tex" )
 	
 	inst.components.health:SetMaxHealth(150)
 	inst.components.hunger:SetMax(150)
 	inst.components.sanity:SetMax(200)
+
+	inst.components.combat.damagemultiplier = .75
 	
 end
 
